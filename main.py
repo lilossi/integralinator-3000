2a326175a639933779c96488240c3962e51bab31
Oscar Philippow <code@philippow.com>
2026-03-22 14:30:05 +0100

    saved expression depths

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sympy import *
 from evaluation.controllability import get_controllability_score
 from evaluation.evaluation import print_entire_evaluation
+from evaluation.expression_depth import get_expression_depth
 from test_suite.test_integrals import *
 from sympy.integrals.manualintegrate import manualintegrate
 from sympy.integrals.manualintegrate import integral_steps
@@ -8,7 +9,7 @@ from sympy.integrals.manualintegrate import integral_steps
 from utils.tree_solution import get_solution_vector
 def main():
     for integral in SOLVABLE_EXPRESSIONS:
-        print(get_solution_vector(integral))
+        print(get_expression_depth(integral))
 
 if __name__ == "__main__":
     main()
