fd14c47276f77f67331a3b266d6bcd1769d2930e
Oscar Philippow <code@philippow.com>
2026-03-22 13:30:06 +0100

    computed controllability scores for all solvable integrals

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from sympy import *
+from evaluation.controllability import get_controllability_score
 from evaluation.evaluation import print_entire_evaluation
 from test_suite.test_integrals import *
-
+from sympy.integrals.manualintegrate import manualintegrate
+from sympy.integrals.manualintegrate import integral_steps
 def main():
-    print_entire_evaluation(UNSOLVED_EXPRESSIONS[-1])
-            
+    for integral in SOLVABLE_EXPRESSIONS:
+        print(get_controllability_score(integral))
 
 if __name__ == "__main__":
     main()
